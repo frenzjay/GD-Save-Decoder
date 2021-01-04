6b74eab135a5a142daaa05d1d3a295cf6052d927
frenzjay <67408909+frenzjay@users.noreply.github.com>
2021-01-04 20:41:06 +0800

    Update decodeLevels.js

--- decodeLevels.js
+++ decodeLevels.js
@@ -2,7 +2,7 @@ const fs = require('fs')
 const Crypto = require('./crypto.js')
 let crypto = new Crypto()
 
-let gdSave = process.env.HOME || process.env.USERPROFILE + /frenzjay/GD-Save-Decoder/blob/master/CCLocalLevels.dat"
+let gdSave = process.env.HOME || process.env.USERPROFILE + "/frenzjay/GD-Save-Decoder/blob/master/CCLocalLevels.dat"
 
 fs.readFile(gdSave, 'utf8', function (err, saveData) {
     if (err) return console.log("Error! Could not open or find GD save file")
